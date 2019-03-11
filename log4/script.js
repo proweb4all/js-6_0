e4b23de7084738813471fd50b4d0c684c25a6a8b
proweb4all <proweb4all@narod.ru>
2019-03-12 01:31:24 +0300

    log4

---
+++ log4/script.js
@@ -0,0 +1,24 @@
+func(1,5,0,9,15,9,-3,2,-3,5,2,9);
+
+function func(...arr){
+    let calc = [], res = [], countNum;
+    console.log('Вход:  ', arr);
+    for (let i = 0; i < arr.length; i++) {  
+        if (arr[i] != null) {
+            countNum = 1;
+            for (let j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    countNum++;
+                    arr[j] = null;
+                };
+            };
+            calc.push([arr[i],countNum]);
+        };
+    };
+    calc.sort(function(a, b){return b[1] - a[1]});
+    calc.forEach(function(item){
+        for(let i = 0; i < item[1]; i++){res.push(item[0])};
+    });
+    console.log('Выход: ', res);
+    return res;
+};
