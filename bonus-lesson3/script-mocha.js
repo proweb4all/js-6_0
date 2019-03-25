59959056b8a59ce9e32f67a7a6e9abede91fe3b4
proweb4all <proweb4all@narod.ru>
2019-03-25 15:52:22 +0300

    bonus-lesson3

---
+++ bonus-lesson3/script-mocha.js
@@ -0,0 +1,45 @@
+// Функция sum должна возвращать тип данных true. Проверить её на это.
+function sum(a, b) {
+	return a + b > 10;
+}
+sum(2,2);
+
+// Переменная num должна быть равна 5. Проверить на соответствие.
+let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+let num = arr[1][1];
+
+// Узнать, что нам вернет функция each в данных условиях. Проверить её на тип данных, который она возвращает,
+// на соответствие ожидаемому результату (который вы получили) и на свойство length.
+var each = function(startArr, f){return f(startArr)};
+var arr1 = [64, 49, 36, 25, 16];
+var myFunc = function(a){
+	var newArr = [];
+	for (var i = 0; i < a.length; i++) {
+		newArr[i]=Math.sqrt(a[i]);
+	}
+	return newArr;
+}
+//console.log(each(arr1, myFunc));
+
+
+// ============ Тесты Mocha
+console.log('===== Тесты Mocha =====');
+let assert = require('chai').assert;
+describe('Тест1', function(){
+    it ('Функция sum возвращает True.', function(){
+        assert.typeOf(sum(2, 2), 'true');
+    });
+});
+describe('Тест2', function(){
+    it ('Переменная num = 5.', function(){
+        assert.equal(num, 5);
+    });
+});
+describe('Тест3', function(){
+    it ('Функция each возвращает array', function(){
+        assert.typeOf(each(arr1, myFunc), 'array');
+	});
+    it ('Функция each возвращает array длиной 5', function(){
+        assert.equal(each(arr1, myFunc).length, 5);
+    });
+});
---
+++ bonus-lesson3/scritps.test.js
@@ -0,0 +1,21 @@
+// ============ Тесты Jest
+const scriptIn = require('./script');
+
+describe('Тест1.', () => {
+    test('Функция sum возвращает True.', () => {
+        expect(scriptIn.sum).toEqual(true);
+    });
+});
+describe('Тест2.', () => {
+    test('Переменная num = 5.', () => {
+        expect(scriptIn.num).toBe(5);
+    });
+});
+describe('Тест3.', () => {
+    test('Функция each возвращает array (object)', () => {
+        expect(typeof(scriptIn.arr)).toEqual('object');
+    });
+    test('Функция each возвращает array длиной 5', () => {
+        expect(scriptIn.arr.length).toBe(5);
+    });
+});
